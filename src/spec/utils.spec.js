9bd3d29572f27e1154eaad37773e20d711ac5860
Harry Keyes <harryakeyes7@gmail.com>
2020-02-14 17:34:35 +0000

    tested and implemented validateInput and makeNameObj

--- src/spec/utils.spec.js
+++ src/spec/utils.spec.js
@@ -1,4 +1,4 @@
-import { formatJoke } from "../utils";
+import { formatJoke, validateInput, makeNameObj } from "../utils";
 
 describe("formatJoke", () => {
   test("returns a string", () => {
@@ -12,5 +12,53 @@ describe("formatJoke", () => {
   });
   test("returns the passed string with all double quotes formatted", () => {
     expect(formatJoke("a &quot;joke&quot;")).toEqual('a "joke"');
+  });
+});
+
+describe("validateInput", () => {
+  test("returns a boolean", () => {
+    expect(validateInput("")).toEqual(expect.any(Boolean));
+  });
+
+  test("returns false if input is not 2 word", () => {
+    expect(validateInput("Harry Keyes")).toEqual(true);
+    expect(validateInput("Harry")).toEqual(false);
+    expect(validateInput("A person with a very long name")).toEqual(false);
+    expect(validateInput(" Harry Keyes ")).toEqual(true);
+  });
+
+  test("returns false if input contains non letter or string characters", () => {
+    expect(validateInput("Harry1 Keyes")).toEqual(false);
+    expect(validateInput("Harry Keyes!")).toEqual(false);
+    expect(validateInput("'Harry' 'Keyes'")).toEqual(false);
+    expect(validateInput("Harry K3y3s")).toEqual(false);
+  });
+});
+
+describe("makeNameObj", () => {
+  test("returns an object", () => {
+    expect(makeNameObj("Harry Keyes")).toEqual(expect.any(Object));
+  });
+
+  test("returns an object with firstName and lastName keys", () => {
+    const nameObj = makeNameObj("Harry Keyes");
+    expect(nameObj).toHaveProperty("firstName");
+    expect(nameObj).toHaveProperty("lastName");
+  });
+
+  test("returns an object with the input's first and last name assigned to the firstName and lastName keys", () => {
+    const nameObj = makeNameObj("Harry Keyes");
+    expect(nameObj).toHaveProperty("firstName", "Harry");
+    expect(nameObj).toHaveProperty("lastName", "Keyes");
+  });
+
+  test("returns an object with the input's first and last name capitalised", () => {
+    let nameObj = makeNameObj("harry keyes");
+    expect(nameObj).toHaveProperty("firstName", "Harry");
+    expect(nameObj).toHaveProperty("lastName", "Keyes");
+
+    nameObj = makeNameObj("HarrY kEyEs");
+    expect(nameObj).toHaveProperty("firstName", "Harry");
+    expect(nameObj).toHaveProperty("lastName", "Keyes");
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
 export const formatJoke = joke => {
   return joke.replace(/&quot;/g, '"');
 };
+
+export const validateInput = input => {
+  return /^[a-zA-Z]+ [a-zA-Z]+$/.test(input.trim());
+};
+
+export const makeNameObj = input => {
+  const names = input.split(" ");
+  const capitalisedNames = names.map(name => {
+    return name[0].toUpperCase() + name.slice(1).toLowerCase();
+  });
+  return { firstName: capitalisedNames[0], lastName: capitalisedNames[1] };
+};
