b9a406ae3eef45944c084bc2e65a0b2450a92da3
Harry Keyes <harryakeyes7@gmail.com>
2020-02-14 19:34:20 +0000

    added router to app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import RandomJokePage from "./components/RandomJokePage";
 import { fetchRandomJoke } from "./api";
+import { Router } from "@reach/router";
+import SearchJokePage from "./components/SearchJokePage";
 
 function App() {
   return (
     <div className="App">
-      <RandomJokePage fetchRandomJoke={fetchRandomJoke} />
+      <Router>
+        <RandomJokePage path="/" fetchRandomJoke={fetchRandomJoke} />
+        <SearchJokePage path="/search" fetchRandomJoke={fetchRandomJoke} />
+      </Router>
     </div>
   );
 }
