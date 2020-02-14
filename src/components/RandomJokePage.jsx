9ad6c3238c31ea59e79a464fe66a1eb3a8f17c83
Harry Keyes <harryakeyes7@gmail.com>
2020-02-14 10:06:22 +0000

    wrote unit test for error handling and implemented error handling

--- src/components/RandomJokePage.jsx
+++ src/components/RandomJokePage.jsx
@@ -3,25 +3,33 @@ import React, { Component } from "react";
 class RandomJokePage extends Component {
   state = {
     joke: "",
-    isLoading: false
+    isLoading: false,
+    error: false
   };
 
   handleRandomJokeClick = () => {
     this.setState({ isLoading: true });
-    this.props.fetchRandomJoke().then(joke => {
-      this.setState({ joke, isLoading: false });
-    });
+    this.props
+      .fetchRandomJoke()
+      .then(joke => {
+        this.setState({ joke, isLoading: false, error: false });
+      })
+      .catch(() => {
+        this.setState({ error: true, isLoading: false });
+      });
   };
 
   render() {
-    const { joke, isLoading } = this.state;
+    const { joke, isLoading, error } = this.state;
     return (
       <div>
         <button id="randomJokeButton" onClick={this.handleRandomJokeClick}>
           Random Joke
         </button>
         {isLoading ? (
-          <p className="joke">Loading</p>
+          <p className="loading">Loading</p>
+        ) : error ? (
+          <p className="errorMsg">Sorry, could not load a joke at this time.</p>
         ) : (
           <p className="joke">{joke}</p>
         )}
--- src/spec/randomJokePage.spec.js
+++ src/spec/randomJokePage.spec.js
@@ -3,6 +3,7 @@ import React from "react";
 import RandomJokePage from "../components/RandomJokePage";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import waitForExpect from "wait-for-expect";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -14,7 +15,7 @@ test("Checks there is a button with text 'Random Joke' present", () => {
   expect(button.text()).toBe("Random Joke");
 });
 
-test("Checks function is called when button is clicked", () => {
+test("Checks function is called when button is clicked", async () => {
   const successResult = "Returned data";
   const successfulApiCall = jest.fn(() => Promise.resolve(successResult));
 
@@ -22,7 +23,7 @@ test("Checks function is called when button is clicked", () => {
     <RandomJokePage fetchRandomJoke={successfulApiCall} />
   );
 
-  let joke = wrapper.find(".joke");
+  const joke = wrapper.find(".joke");
   expect(joke.text()).toEqual("");
 
   let button = wrapper.find("#randomJokeButton");
@@ -30,6 +31,29 @@ test("Checks function is called when button is clicked", () => {
 
   expect(successfulApiCall.mock.calls.length).toBe(1);
 
-  joke = wrapper.find(".joke");
-  expect(joke.text()).toEqual("Loading");
+  const loading = wrapper.find(".loading");
+  expect(loading.text()).toEqual("Loading");
+
+  await waitForExpect(() => {
+    const newJoke = wrapper.find(".joke");
+    expect(newJoke.text()).toEqual("Returned data");
+  });
+});
+
+test("Checks an error message is displayed when the API call fails", async () => {
+  const badApiCall = jest.fn(() => Promise.reject(new Error()));
+
+  const wrapper = shallow(<RandomJokePage fetchRandomJoke={badApiCall} />);
+
+  let button = wrapper.find("#randomJokeButton");
+  button.simulate("click");
+
+  expect(badApiCall.mock.calls.length).toBe(1);
+
+  await waitForExpect(() => {
+    const errorMsg = wrapper.find(".errorMsg");
+    expect(errorMsg.text()).toEqual(
+      "Sorry, could not load a joke at this time."
+    );
+  });
 });
