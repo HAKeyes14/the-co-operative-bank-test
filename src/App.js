76059ac2d1bced620d862db9ed7457d8ce3034a7
Harry Keyes <harryakeyes7@gmail.com>
2020-02-14 20:03:08 +0000

    added header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import RandomJokePage from "./components/RandomJokePage";
 import { fetchRandomJoke } from "./api";
 import { Router } from "@reach/router";
 import SearchJokePage from "./components/SearchJokePage";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Navbar />
       <Router>
         <RandomJokePage path="/" fetchRandomJoke={fetchRandomJoke} />
         <SearchJokePage path="/search" fetchRandomJoke={fetchRandomJoke} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1>The Co-operative Bank Tech Test</h1>
+      <h2>A Joke Generator</h2>
+    </>
+  );
+};
+
+export default Header;
