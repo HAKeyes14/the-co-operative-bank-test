d15ca37c03c12217bc6b012f0dc82dd41a24f9d3
Harry Keyes <harryakeyes7@gmail.com>
2020-02-14 18:25:49 +0000

    tested and implemented fetchRandomJoke returning a passed in name

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const fetchRandomJoke = async () => {
-  const { data } = await axios.get("https://api.icndb.com/jokes/random");
+export const fetchRandomJoke = async nameObj => {
+  const { data } = await axios.get("https://api.icndb.com/jokes/random", {
+    params: nameObj
+  });
   return data.value.joke;
 };
--- src/spec/api.spec.js
+++ src/spec/api.spec.js
@@ -1,5 +1,20 @@
 import { fetchRandomJoke } from "../api";
 
-test("returns an string", async () => {
-  await expect(fetchRandomJoke()).resolves.toStrictEqual(expect.any(String));
+describe("fetchRandomJoke", () => {
+  test("returns an string", async () => {
+    await expect(fetchRandomJoke()).resolves.toEqual(expect.any(String));
+  });
+
+  test("the returned string contains 'Chuck Norris' if no argument is passed", async () => {
+    await expect(fetchRandomJoke()).resolves.toEqual(
+      expect.stringContaining("Chuck Norris")
+    );
+  });
+
+  test("the returned string contains the passed in first and last name", async () => {
+    const nameObj = { firstName: "Harry", lastName: "Keyes" };
+    await expect(fetchRandomJoke(nameObj)).resolves.toEqual(
+      expect.stringContaining("Harry Keyes")
+    );
+  });
 });
