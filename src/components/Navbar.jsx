558ee0c2f99705dc94fe88f9626d1fb58a359393
Harry Keyes <harryakeyes7@gmail.com>
2020-02-15 10:23:44 +0000

    added active class to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "@reach/router";
 
-const Navbar = () => {
-  return (
-    <nav className="Navbar">
-      <Link to="/">
-        <p>Random Jokes</p>
-      </Link>
-      <Link to="/search">
-        <p>Search Jokes</p>
-      </Link>
-      <Link to="/list">
-        <p>Jokes List</p>
-      </Link>
-    </nav>
-  );
-};
+class Navbar extends Component {
+  state = {
+    active: "random"
+  };
+
+  render() {
+    const { active } = this.state;
+    return (
+      <nav className="Navbar">
+        <Link
+          to="/"
+          onClick={() => {
+            this.setState({ active: "random" });
+          }}
+          className={active === "random" && "active"}
+        >
+          <p>Random Jokes</p>
+        </Link>
+        <Link
+          to="/search"
+          onClick={() => {
+            this.setState({ active: "search" });
+          }}
+          className={active === "search" && "active"}
+        >
+          <p>Search Jokes</p>
+        </Link>
+        <Link
+          to="/list"
+          onClick={() => {
+            this.setState({ active: "list" });
+          }}
+          className={active === "list" && "active"}
+        >
+          <p>Jokes List</p>
+        </Link>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
